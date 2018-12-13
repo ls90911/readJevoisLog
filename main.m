78084394aa9c722931a65452f54948b2a9717072
Shuo Li <ls90911@gmail.com>
2018-12-13 11:32:26 +0100

    log 7470. Add time stamp to both ransac log and jevois log

--- main.m
+++ main.m
@@ -5,21 +5,21 @@ clear global
 
 global m n OB RANSAC
 %--------------------------------------
-file_name = '7685.csv';  % 7224
+file_name = '7470.csv';  % 7224
 %--------------------------------------
 import_data(file_name);
-[RANSAC] = import_ransac_data();
+%[RANSAC] = import_ransac_data();
 
 
-% m = 1;
-% n = length(OB.TIME);
-
- m = 2402;
- n = 3102;
+m = 1;
+n = length(OB.TIME);
+% 
+%  m = 2402;
+%  n = 3102;
 
 
 plot_log();
 
-analysis_ransac_file(72)
+%analysis_ransac_file(72)
 
 temp = 1;
